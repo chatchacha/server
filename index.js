3cb4d133f5fd89a4322977c458b6caa886b775ff
Anis Safine <anis@safine.me>
2016-09-06 12:52:06 +0200

    Add error management

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-var config = require('./config.js');
-require('socket.io').listen(config.port);
+// error management
+process.on('uncaughtException', function(err) {
+    console.log('An unknown error occured', err);
+    process.exit(1);
+});
+
+// get the configuration
+try {
+    var config = require('./config.js');
+} catch (e) {
+    console.log('Failed to import the configuration');
+    process.exit(1);
+}
+
+// try to start the server
+try {
+    var io = require('socket.io').listen(config.port);
+} catch (e) {
+    console.log('Failed to start the socket server');
+    process.exit(1);
+}
