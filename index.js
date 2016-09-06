616404f0d98149938f9f40ea70ec46270705ac5e
Anis Safine <anis@safine.me>
2016-09-06 14:25:52 +0200

    Improve error management
    - Generate log file with complete stack of the error
    - Improve the output with a few colors

--- index.js
+++ index.js
@@ -1,21 +1,56 @@
-// error management
-process.on('uncaughtException', function(err) {
-    console.log('An unknown error occured', err);
-    process.exit(1);
-});
+// get utilities
+try {
+    process.stdout.write('- Loading dependencies... ');
+
+    var colors = require('colors');
+    var fs = require('fs');
+
+    console.log('OK'.green);
+} catch (e) {
+    handleError(e);
+}
 
 // get the configuration
 try {
+    process.stdout.write('- Importing local configuration... ');
+
     var config = require('./config.js');
+
+    console.log('OK'.green);
 } catch (e) {
-    console.log('Failed to import the configuration');
-    process.exit(1);
+    handleError(e);
 }
 
 // try to start the server
 try {
+    process.stdout.write('- Starting socket.io... ');
+
     var io = require('socket.io').listen(config.port);
+    io.sockets.on('ready', function () {
+        console.log('OK'.green);
+    });
 } catch (e) {
-    console.log('Failed to start the socket server');
+    handleError(e);
+}
+
+// error management
+process.on('uncaughtException', function(e) {
+    if (io) {
+        io.close();
+    }
+
+    handleError(e);
+});
+
+/**
+ * Handles an exception
+ *
+ * @param {Object} e Exception
+ */
+function handleError(e) {
+    console.log('FAIL'.red + "\n");
+    console.log(e.message.bgRed);
+    console.log('Please make sure you followed the installation instructions in README.md. For more details, please refer to error.log'.bgRed);
+    fs.writeFileSync('error.log', e.stack);
     process.exit(1);
 }
